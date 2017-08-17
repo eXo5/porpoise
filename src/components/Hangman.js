4b24429cc52aceff35136f3bf68676db7f6b04b4
eXo5 <mike.yingling15@gmail.com>
2017-08-16 23:27:28 -0400

    half operational constructor

---
+++ src/components/Hangman.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import { Route, Link } from 'react-router-dom'
+import { Button } from 'react-materialize'
+import helper from '../utility/helper/helper'
+
+class Hangman extends Component {
+	constructor(){
+	super()
+	
+	}
+
+	render(){
+		return(
+			
+		)
+	}
+
+}
--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -5,57 +5,95 @@ class Trivia extends Component {
 	constructor(props){
 		super(props)
 		this.state = {
-			triviaQs: [], 
-			triviaQ1: ""
 
+			triviaQs: [], 
+			triviaQ1: "",
+			triq: [1, 2, 3, 4, 5]
 		}
 	}
 
 mapQs = () => {
 return this.state.triviaQs.map((element, i) => {
+	console.log(element.category)
 	return(
 		<span key={i}>
-			<h2>{element.category}</h2>
+			<h2>element.category</h2>
 		</span>
 		)
 	})
 
 }
 
+// showQs = () => { this.state.triviaQs.map((element, i ) => {
+// 					console.log(element.type)
+// 	return(
+// 						<div key={i}>
+// 							<p>{element.category}</p>
+// 						</div>	
+// 					)			
+// 	})
+// }
 componentDidMount(){
-	var qList = [];
+	var qList = []
+	class Question {
+		constructor(category, type, difficulty, correct_answer, incorrect_answers, question) {
+				this.category = "",
+				this.type = "",
+				this.difficulty = "",
+				this.correct_answer = "",
+				this.incorrect_answers = [],
+				this.question = ""
+			}
+		};
 	helper.getQs()
 		.then(function(results) {
 			results.data.results.map(function(element, i) {
-				return qList.push(element)
-			})
+				
+				var	newQuestion = new Question(
+						`${element.category}`,
+						`${element.type}`,
+						`${element.difficulty}`,
+						`${element.correct_answer}`,
+						[],
+						`${element.question}`
+					)
+					element.incorrect_answers.map(function(element, i){
+						newQuestion.incorrect_answers.push({element})
+					})
+					qList.push(newQuestion)
+				})								
 			//console.log(qList)
 			//console.log("^^^qList")
-			console.log(results)
-			console.log("^^RESULTS^^")
+			//console.log(results)
+			//console.log("^^RESULTS^^")
 		})
-		this.setState({triviaQs: qList});
-	}
+		this.setState({triviaQs: qList})
+}
+
+
 componentDidUpdate(){
 				console.log(this.state.triviaQs)
 				console.log("^^^state.triviaQs")
+				console.log(this.triq)
 }
+
 	render() {
 		console.log(this.state.triviaQs)
 		console.log("am I fucking stupid")
-				const showQs =  this.state.triviaQs.map((element, i ) => {
-					console.log(element.type)
-					return(
-						<div key={i}>
-							<p>{element.category}</p>
-						</div>	
-					)
-	})
-				var showQ1 = () => { 
-					return ( <h1>{this.state.triviaQs[0].category}</h1>
-						)
-				}
 
+			var showQs = this.state.triviaQs.map((element, i) => {
+				return(
+					<div key={i}>
+						<p>{this.state.triviaQs[`${i}`].category}</p>
+					</div>
+					)
+			})
+
+				let showNums = this.state.triq.map((element, i) => {
+					return(
+						<p>{element}</p>
+						)
+				})
 	return (
 			<div>
 				{showQs}
